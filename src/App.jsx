b0777ae692672638a78542bdaa92a7136af37f67
Otabekfrontend <otabekmadaminov1022@gmail.com>
2024-08-16 06:17:37 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axiosClient("countries")
+        axiosClient("/countries")
             .then((data) => {
                 console.log(data.data.data);
                 setCountries(data.data.data);
--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let mainUrl = "https://json-api.uz/api/project/otabek-todo-list/";
+let mainUrl = "https://json-api.uz/api/project/otabek-todo-list";
 
 export const axiosClient = axios.create({
     baseURL: mainUrl,
